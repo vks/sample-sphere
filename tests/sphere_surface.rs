723885d9548137d6f084673a2ba4aa6ccc24b05c
Vinzent Steinberg <Vinzent.Steinberg@gmail.com>
2018-07-24 15:40:32 +0200

    Move common macro to its own file

--- tests/sphere_surface.rs
+++ tests/sphere_surface.rs
@@ -2,24 +2,10 @@ extern crate rand;
 
 extern crate sample_sphere;
 
+#[macro_use] mod macros;
+
 use rand::FromEntropy;
 use sample_sphere::sphere_surface;
-
-/// Assert that two numbers are almost equal to each other.
-///
-/// On panic, this macro will print the values of the expressions with their
-/// debug representations.
-macro_rules! assert_almost_eq {
-    ($a:expr, $b:expr, $prec:expr) => (
-        let diff = ($a - $b).abs();
-        if diff > $prec {
-            panic!(format!(
-                "assertion failed: `abs(left - right) = {:.1e} < {:e}`, \
-                 (left: `{}`, right: `{}`)",
-                diff, $prec, $a, $b));
-        }
-    );
-}
 
 macro_rules! make_norm_test {
     ($name:ident, $fun:path) => (
